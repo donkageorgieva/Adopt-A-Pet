0ab2b7496dc44d341deb48d04fe0cc80e22f3e8a
Donka Georgieva <65857282+donkageorgieva@users.noreply.github.com>
2021-03-12 16:22:58 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.scss';
+import Header from './components/Header/Header'
+
+
+
+function App() {
+  return (
+    <div className="App">
+     <Header></Header>
+    </div>
+  );
+}
+
+export default App;
