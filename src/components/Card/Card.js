e12052544fa0281a87b07a3f60403b2c456a64ee
Donka Georgieva <65857282+donkageorgieva@users.noreply.github.com>
2021-03-17 13:34:22 +0200

    Add files via upload

---
+++ src/components/Card/Card.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Card} from 'react-bootstrap';
+
+const card = (props) => {
+
+let catInfo;
+if (props.cat){
+    catInfo =   <React.Fragment>
+           <Card.Img variant="top" src={props.imgURL} />
+        <Card.Title>{ props.cat.name}</Card.Title>
+    <Card.Text>{ props.cat.description}</Card.Text>
+    <Card.Text></Card.Text>
+        </React.Fragment>
+}
+
+
+return (
+
+
+    <Card>
+        <Card.Img variant="top" />
+        <Card.Body>
+
+          {catInfo}
+            <Card.Text></Card.Text>
+            <Card.Text></Card.Text>
+         
+        </Card.Body>
+    </Card>
+)
+}
+
+
+
+
+export default card;
